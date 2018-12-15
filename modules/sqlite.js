d1e2772f17e6f325e8dc7a7ec05f8f3cd69d1627
Giuseppe Fagnani <gfagnani.job@gmail.com>
2018-12-15 19:25:59 +0100

    add error handling on connection to db

--- modules/sqlite.js
+++ modules/sqlite.js
@@ -1,5 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('./db/chinook.db');
+let db = new sqlite3.Database('./db/chinook.db', (err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log("Connected to chinook database");
+  }
+});
 
 module.exports = db;
